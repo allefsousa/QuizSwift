f161616e9dacecd9ce95ffe6dd04960166c500b0
José Guilherme Scanavini <development@izagro.com.br>
2018-10-19 17:13:54 -0300

    quiz swift bacana

--- SwiftQuiz/QuizViewController.swift
+++ SwiftQuiz/QuizViewController.swift
@@ -12,13 +12,58 @@ import UIKit
 
 class QuizViewController: UIViewController {
 
+    @IBOutlet weak var vTimer: UIView!
     @IBOutlet var btAnswer: [UIButton]! // array de botoes da view
     @IBOutlet weak var lbQuestion: UILabel! // label com as questões
     @IBOutlet weak var viTimer: UILabel! // label que é o timer
+    
+    let quizManager = QuizManager()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+    }
+    // metodo inicia quando a view é carregada
+    override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+        
+        vTimer.frame.size.width = view.frame.size.width
+        UIView.animate(withDuration: 60.0, delay: 0, options: .curveLinear, animations: {
+            self.vTimer.frame.size.width = 0
+        }) { (success) in
+            self.showResults()
+        }
+        
+        
+        getNewQuiz()
+        
+        
+        
+    }
+    // exibindo resultado do quiz
+    func showResults(){
+        // chamando uma nova tela. Porem para isso foi atribuido um nome a segue para que a mesma possa ser acessada via codigo
+        performSegue(withIdentifier: "resultSegue", sender: nil)
+        
+    }
+    // metodo chamada pelo sistema antes da nova tela aparecer
+    // onde os dados são passados para a proxima tela
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let resultViewController = segue.destination as!
+        ResultViewController
+        resultViewController.totalAnwers = quizManager.totalAnswers
+        resultViewController.totalCorrectAnswers = quizManager.totalCorrectAnswers
+    }
+    
+    // iniciar um novo quiz
+    func getNewQuiz(){
+       quizManager.refresh()
+        lbQuestion.text = quizManager.questions
+            // percorrendo as respostas para adicionar aos botoes da view
+        for i in 0..<quizManager.options.count{
+            let options = quizManager.options[i]
+            let button = btAnswer[i]
+            button.setTitle(options, for: .normal)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,18 +72,14 @@ class QuizViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
     
-    // relação de ações do Botão
+    // relação de ações do Botão que vai scorresponder ao botão selecionado
     @IBAction func selectAnswer(_ sender: UIButton) {
+        
+        let index = btAnswer.index(of: sender)!
+        quizManager.validateAnswers(index: index)
+            getNewQuiz()
+        
+        
     }
 }
--- SwiftQuiz/ResultViewController.swift
+++ SwiftQuiz/ResultViewController.swift
@@ -17,18 +17,32 @@ class ResultViewController: UIViewController {
     @IBOutlet weak var lberror: UILabel!
     
     @IBOutlet weak var lbScore: UILabel!
+    
+    
+    // variaveis que vão receber os dados da tela anterior
+    var totalCorrectAnswers: Int = 0
+    var totalAnwers: Int = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        lbAnswered.text = "Perguntas respondidas: \(totalAnwers)"
+                lbCorrect.text = "Perguntas corretas: \(totalCorrectAnswers)"
+                lberror.text = "Perguntas erradas: \(totalAnwers - totalCorrectAnswers)"
+        
+        let score = totalCorrectAnswers * 100/totalAnwers
+        lbScore.text = "\(score)%"
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+    // fechando a tela
     @IBAction func close(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
   
 }
