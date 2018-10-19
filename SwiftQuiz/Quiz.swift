221bf61241233683cc11aa5110d66e2960028625
José Guilherme Scanavini <development@izagro.com.br>
2018-10-19 15:36:07 -0300

    Classe de modelo implementada

--- SwiftQuiz/Quiz.swift
+++ SwiftQuiz/Quiz.swift
@@ -8,6 +8,29 @@
 
 import Foundation
 
+
+// model
 class Quiz{
+    let questions:String
+    let options:[String]
+    private let correctdAnswer:String
+    
+    init(question:String,options:[String],correctedAnswer:String) {
+        self.questions = question
+        self.options = options
+        self.correctdAnswer = correctedAnswer
+    }
+    
+    // validando pergunta e retornando se o usuari oacertou ou não
+    func validateOption(_ index:Int) -> Bool {
+        let answer = options[index]
+        return answer == correctdAnswer
+    }
+    
+    deinit {
+        print("Liberou quis da memoria")
+    }
+    
+    
     
 }
